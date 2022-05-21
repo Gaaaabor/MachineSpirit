624193cbf6f2b773fc18142ca5a7828d9db755c4
Enrius <bocsgab@gmail.com>
2022-05-22 00:09:02 +0200

    Update DeviceAttachment.cpp

--- DeviceAttachment.cpp
+++ DeviceAttachment.cpp
@@ -1,64 +1,64 @@
-// DeviceAttachment.cpp 
- #include "DeviceAttachment.h" 
-  
- DeviceAttachment::DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin) 
- {     
-   id = id;
+// DeviceAttachment.cpp
+#include "DeviceAttachment.h"
+
+DeviceAttachment::DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin)
+{
+   id = id;
    ownerUserId = ownerUserId;
-   ownerDeviceSerial = ownerDeviceSerial; 
-   attachmentName = attachmentName; 
-   serial = serial; 
-   capability = capability; 
-   powerPin = powerPin; 
-  
+   ownerDeviceSerial = ownerDeviceSerial;
+   attachmentName = attachmentName;
+   serial = serial;
+   capability = capability;
+   powerPin = powerPin;
+
    // Not sure
-   analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0 
-    
-   pinMode(powerPin, OUTPUT); 
- } 
-  
- float DeviceAttachment::measure()  
- { 
-   if(capability != 2) // Capability 2 is Measure
-   {
-     return 0;
-   }
+   analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 
-   digitalWrite(powerPin, HIGH); 
-   delay(200); 
-   int analogMeasurement = analogRead(analogInPin); 
-   digitalWrite(powerPin, LOW);
-   measureState = (float)analogMeasurement;
-   return measureState; 
- } 
-  
- bool DeviceAttachment::toggle()  
- { 
-   if(capability != 0) // Capability 0 is BinarySwitch
-   {
-     return false;
-   }
+   pinMode(powerPin, OUTPUT);
+}
 
-   if(switchState) 
-   { 
-     digitalWrite(powerPin, LOW);
-   }
-   else
-   {
-     digitalWrite(powerPin, HIGH);
-   }
-   
-   switchState = !switchState;
-   return switchState;   
- } 
-  
- float DeviceAttachment::dim(float value)
- {
-   if(capability != 1) // Capability 1 is Dim
-   {
-     return 0;
-   }
+float DeviceAttachment::measure()
+{
+  if(capability != 2) // Capability 2 is Measure
+  {
+    return 0;
+  }
 
-   dimState = value; 
-   return dimState; 
- }
+  digitalWrite(powerPin, HIGH);
+  delay(200);
+  int analogMeasurement = analogRead(analogInPin);
+  digitalWrite(powerPin, LOW);
+  measureState = (float)analogMeasurement;
+  return measureState;
+}
+
+bool DeviceAttachment::toggle()
+{
+  if(capability != 0) // Capability 0 is BinarySwitch
+  {
+    return false;
+  }
+
+  if(switchState)
+  {
+    digitalWrite(powerPin, LOW);
+  }
+  else
+  {
+    digitalWrite(powerPin, HIGH);
+  }
+
+  switchState = !switchState;
+  return switchState;
+}
+
+float DeviceAttachment::dim(float value)
+{
+  if(capability != 1) // Capability 1 is Dim
+  {
+    return 0;
+  }
+
+  dimState = value;
+  return dimState;
+}
