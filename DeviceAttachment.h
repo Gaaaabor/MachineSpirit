75f42687176e3003e230f842176e2be5f6a7358b
Enrius <bocsgab@gmail.com>
2022-05-21 23:56:35 +0200

    Update DeviceAttachment.h

--- DeviceAttachment.h
+++ DeviceAttachment.h
@@ -1,28 +1,28 @@
-// DeviceAttachment.h 
- #ifndef DeviceAttachment_h 
- #define DeviceAttachment_h 
-  
- #include <Arduino.h> 
-  
- class DeviceAttachment { 
-   private: 
-     String id; 
+// DeviceAttachment.h
+#ifndef DeviceAttachment_h
+#define DeviceAttachment_h
+
+#include <Arduino.h>
+
+class DeviceAttachment
+{
+   private:
+     String id;
      String ownerUserId;
-     String ownerDeviceSerial; 
-     String attachmentName; 
-     String serial; 
-     float measureState;
+     String ownerDeviceSerial;
+     String attachmentName;
+     String serial;
+     float measureState;
      bool switchState;
      float dimState;
-     byte capability; 
-     byte powerPin; 
-     byte analogInPin; 
-  
-   public: 
-     DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin); 
-     float measure(); 
-     bool toggle();     
-     float dim(float value);     
- }; 
-  
- #endif
+     byte capability;
+     byte powerPin;
+     byte analogInPin;
+
+   public:
+     DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin);
+     float measure();
+     bool toggle();
+     float dim(float value);
+};
+#endif
