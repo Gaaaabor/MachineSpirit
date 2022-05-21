b36e069a959605c70913ea867a08dfdb1ce7413e
Enrius <bocsgab@gmail.com>
2022-05-21 13:46:28 +0200

    Create DeviceAttachment.h

---
+++ DeviceAttachment.h
@@ -0,0 +1,26 @@
+// DeviceAttachment.h 
+ #ifndef DeviceAttachment_h 
+ #define DeviceAttachment_h 
+  
+ #include <Arduino.h> 
+  
+ class DeviceAttachment { 
+   private: 
+     String id; 
+     String ownerUserId;
+     String ownerDeviceSerial; 
+     String attachmentName; 
+     String serial; 
+     String state; 
+     String capability; 
+     int powerPin; 
+     int analogInPin; 
+  
+   public: 
+     DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, String capability, int powerPin); 
+     float measure(); 
+     bool toggle();     
+     float dim(float value);     
+ }; 
+  
+ #endif
