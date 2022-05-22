a2f60e15526ad11da261db5f6d60db765a23b799
Bocsó Gábor <bocsgab@gmail.com>
2022-05-22 12:41:42 +0200

    Code formatting, cleanup

--- DeviceAttachment.cpp
+++ DeviceAttachment.cpp
@@ -3,62 +3,73 @@
 
 DeviceAttachment::DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin)
 {
-   id = id;
-   ownerUserId = ownerUserId;
-   ownerDeviceSerial = ownerDeviceSerial;
-   attachmentName = attachmentName;
-   serial = serial;
-   capability = capability;
-   powerPin = powerPin;
+	id = id;
+	ownerUserId = ownerUserId;
+	ownerDeviceSerial = ownerDeviceSerial;
+	attachmentName = attachmentName;
+	serial = serial;
+	capability = capability;
+	powerPin = powerPin;
 
-   // Not sure
-   analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+	// Not sure
+	analogInPin = A0; // ESP8266 Analog Pin ADC0 = A0
 
-   pinMode(powerPin, OUTPUT);
+	pinMode(powerPin, OUTPUT);
 }
 
 float DeviceAttachment::measure()
 {
-  if(capability != 2) // Capability 2 is Measure
-  {
-    return 0;
-  }
+	if (capability != 2) // Capability 2 is Measure
+	{
+		return 0;
+	}
 
-  digitalWrite(powerPin, HIGH);
-  delay(200);
-  int analogMeasurement = analogRead(analogInPin);
-  digitalWrite(powerPin, LOW);
-  measureState = (float)analogMeasurement;
-  return measureState;
+	digitalWrite(powerPin, HIGH);
+	delay(200);
+	int analogMeasurement = analogRead(analogInPin);
+	digitalWrite(powerPin, LOW);
+	measureState = (float)analogMeasurement;
+	return measureState;
 }
 
 bool DeviceAttachment::toggle()
 {
-  if(capability != 0) // Capability 0 is BinarySwitch
-  {
-    return false;
-  }
+	if (capability != 0) // Capability 0 is BinarySwitch
+	{
+		return false;
+	}
 
-  if(switchState)
-  {
-    digitalWrite(powerPin, LOW);
-  }
-  else
-  {
-    digitalWrite(powerPin, HIGH);
-  }
+	if (switchState)
+	{
+		digitalWrite(powerPin, LOW);
+	}
+	else
+	{
+		digitalWrite(powerPin, HIGH);
+	}
 
-  switchState = !switchState;
-  return switchState;
+	switchState = !switchState;
+	return switchState;
 }
 
 float DeviceAttachment::dim(float value)
 {
-  if(capability != 1) // Capability 1 is Dim
+	if (capability != 1) // Capability 1 is Dim
+	{
+		return 0;
+	}
+
+  if(value > 255)
   {
-    return 0;
+    value = 255;
+  }
+  else if(value < 0)
+  {
+    value = 0;
   }
 
-  dimState = value;
-  return dimState;
+  analogWrite(powerPin, value);
+
+	dimState = value;
+	return dimState;
 }
--- DeviceAttachment.h
+++ DeviceAttachment.h
@@ -6,23 +6,23 @@
 
 class DeviceAttachment
 {
-   private:
-     String id;
-     String ownerUserId;
-     String ownerDeviceSerial;
-     String attachmentName;
-     String serial;
-     float measureState;
-     bool switchState;
-     float dimState;
-     byte capability;
-     byte powerPin;
-     byte analogInPin;
+private:
+	String id;
+	String ownerUserId;
+	String ownerDeviceSerial;
+	String attachmentName;
+	String serial;
+	float measureState;
+	bool switchState;
+	float dimState;
+	byte capability;
+	byte powerPin;
+	byte analogInPin;
 
-   public:
-     DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin);
-     float measure();
-     bool toggle();
-     float dim(float value);
+public:
+	DeviceAttachment(String id, String ownerUserId, String ownerDeviceSerial, String attachmentName, String serial, byte capability, byte powerPin);
+	float measure();
+	bool toggle();
+	float dim(float value);
 };
 #endif
